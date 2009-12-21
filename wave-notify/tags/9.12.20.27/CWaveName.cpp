3a48ee8e268fdcd319c8cc1483a3933383517a09
Pieter van Ginkel <pvginkel@gmail.com>
2009-12-21 12:05:53 +0000

    * Release of 9.12.20.27.

---
+++ wave-notify/tags/9.12.20.27/CWaveName.cpp
@@ -0,0 +1,25 @@
+/*
+ * This file is part of Google Wave Notifier.
+ *
+ * Google Wave Notifier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Google Wave Notifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Google Wave Notifier.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "stdafx.h"
+#include "include.h"
+
+CWaveName::CWaveName(Json::Value & vRoot)
+{
+	m_szName = vRoot[L"1"].asString();
+	m_nType = (WAVE_NAME_TYPE)vRoot[L"2"][L"1"].asUInt();
+}
