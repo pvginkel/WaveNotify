93b34be34785c42c6c1132a907bb480b7821437e
Pieter van Ginkel <pvginkel@gmail.com>
2010-01-07 12:59:58 +0000

    * Created version 10.1.7.36.

---
+++ wave-notify/tags/10.1.7.36/CWaveReader.cpp
@@ -0,0 +1,95 @@
+/*
+ * This file is part of Google Wave Notifier.
+ *
+ * Google Wave Notifier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Google Wave Notifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Google Wave Notifier.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "stdafx.h"
+#include "include.h"
+
+BOOL CWaveReader::Read(LPBYTE lpData, DWORD cbData)
+{
+	m_szBuffer << m_vConverter.Parse(lpData, cbData);
+
+	return PumpResponseBuffer();
+}
+
+BOOL CWaveReader::PumpResponseBuffer()
+{
+	BOOL fSuccess;
+
+	while (PumpMessage(fSuccess) && fSuccess)
+		;
+
+	return fSuccess;
+}
+
+BOOL CWaveReader::PumpMessage(BOOL & fSuccess)
+{
+	wstring szReceived = m_szBuffer.str();
+
+	// Does the current content has a newline?
+
+	DWORD dwPos = szReceived.find(L'\n');
+
+	if (dwPos == wstring::npos)
+	{
+		// No full packet in yet.
+
+		fSuccess = TRUE;
+		return FALSE;
+	}
+
+	wstring szLength = szReceived.substr(0, dwPos);
+
+	if (!iswdigit(szLength))
+	{
+		// The first line was not a number; we cannot read a package.
+
+		fSuccess = FALSE;
+		return FALSE;
+	}
+
+	DWORD dwLength = _wtol(szLength.c_str());
+
+	if (szReceived.length() < dwPos + 1 + dwLength)
+	{
+		// No full packet in yet.
+
+		fSuccess = TRUE;
+		return FALSE;
+	}
+
+	// We have received a full package; parse to a response and submit.
+
+	fSuccess = m_lpSession->ParseChannelResponse(szReceived.substr(dwPos + 1, dwLength));
+
+	m_szBuffer.str(L"");
+
+	if (!fSuccess)
+	{
+		return FALSE;
+	}
+
+	if (szReceived.length() > dwPos + 1 + dwLength)
+	{
+		m_szBuffer << szReceived.substr(dwPos + 1 + dwLength);
+
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
---
+++ wave-notify/tags/10.1.7.36/dialog.h
@@ -0,0 +1,65 @@
+/*
+ * This file is part of Google Wave Notifier.
+ *
+ * Google Wave Notifier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Google Wave Notifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Google Wave Notifier.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _INC_DIALOG
+#define _INC_DIALOG
+
+#pragma once
+
+class CDialog;
+
+class CModelessDialogs
+{
+public:
+	typedef map<INT, CDialog *> THandlesMap;
+	typedef THandlesMap::iterator THandlesMapIter;
+	typedef THandlesMap::const_iterator THandlesMapConstIter;
+
+private:
+	static THandlesMap m_vDialogs;
+
+public:
+	static BOOL IsDialogMessage(LPMSG lpMsg);
+	static void RegisterDialog(INT nType, CDialog * lpDialog);
+	static void UnregisterDialog(INT nType);
+	static void UnregisterDialog(CDialog * lpDialog);
+	static BOOL ContainsType(INT nType);
+	static CDialog * GetDialog(INT nType) { return m_vDialogs[nType]; }
+};
+
+class CDialog : public CWindowHandle
+{
+private:
+	INT m_nResource;
+	BOOL m_fDisposing;
+
+protected:
+	CDialog(INT nResource);
+	virtual ~CDialog();
+
+public:
+	INT_PTR ShowDialog(CWindowHandle * lpParentWindow = NULL);
+	void Create(INT nType, CWindowHandle * lpParentWindow = NULL);
+
+protected:
+	virtual INT_PTR DialogProc(UINT uMessage, WPARAM wParam, LPARAM lParam);
+
+private:
+	static INT_PTR CALLBACK DialogProcCallback(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam);
+};
+
+#endif // _INC_DIALOG
