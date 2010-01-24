a91c99f0602191e112db3668e0bfe36eb2f62c2d
pvginkel <pvginkel@7609e39e-0f13-11df-9e5d-c9c78cbd7d0e>
2010-01-24 10:41:22 +0000

    * Added specialized CDC classes.

--- wave-notify/trunk/gdi.h
+++ wave-notify/trunk/gdi.h
@@ -27,6 +27,8 @@ private:
 
 public:
 	CDC(HDC hDC) { m_hDC = hDC; }
+	virtual ~CDC() { }
+
 	void FrameRect(LPRECT lprc, HBRUSH hBrush) const { ::FrameRect(m_hDC, lprc, hBrush); }
 	void FillRect(LPRECT lprc, HBRUSH hBrush) const { ::FillRect(m_hDC, lprc, hBrush); }
 	void DrawIconEx(int xLeft, int yTop, HICON hIcon, int cxWidth, int cyWidth, UINT istepIfAniCur = 0, HBRUSH hbrFlickerFreeDraw = NULL, UINT diFlags = DI_NORMAL) const {
@@ -38,12 +40,73 @@ public:
 	void GetTextMetrics(LPTEXTMETRIC lptm) const { ::GetTextMetrics(m_hDC, lptm); }
 	void DrawText(wstring szText, LPRECT lprc, UINT format) const { ::DrawText(m_hDC, szText.c_str(), szText.size(), lprc, format); }
 	HDC GetHandle() const { return m_hDC; }
-	void SetTextColor(COLORREF cr) const { ::SetTextColor(m_hDC, cr); }
-	void SetBkColor(COLORREF cr) const { ::SetBkColor(m_hDC, cr); }
+	COLORREF SetTextColor(COLORREF cr) const { return ::SetTextColor(m_hDC, cr); }
+	COLORREF SetBkColor(COLORREF cr) const { return ::SetBkColor(m_hDC, cr); }
+	int SetBkMode(int nMode) const { return ::SetBkMode(m_hDC, nMode); }
 	void MoveToEx(INT x, INT y, LPPOINT lppt = NULL) const { ::MoveToEx(m_hDC, x, y, lppt); }
 	void LineTo(INT x, INT y) const { ::LineTo(m_hDC, x, y); }
 	void BitBlt(POINT ptDest, SIZE szDest, CDC * lpSourceDC, POINT ptSource, DWORD dwRop) const {
 		::BitBlt(m_hDC, ptDest.x, ptDest.y, szDest.cx, szDest.cy, lpSourceDC->GetHandle(), ptSource.x, ptSource.y, dwRop);
+	}
+	INT ExcludeClipRect(LPRECT lprc) {
+		return ::ExcludeClipRect(m_hDC, lprc->left, lprc->top, lprc->right, lprc->bottom);
+	}
+	BOOL GradientFill(PTRIVERTEX pVertex, ULONG dwNumVertex, PVOID pMesh, ULONG dwNumMesh, ULONG dwMode) {
+		return ::GradientFill(m_hDC, pVertex, dwNumVertex, pMesh, dwNumMesh, dwMode);
+	}
+};
+
+class CWindowDC : public CDC
+{
+private:
+	CWindowHandle * m_lpWindow;
+
+public:
+	CWindowDC(CWindowHandle * lpWindow) : m_lpWindow(lpWindow), CDC(GetWindowDC(lpWindow->GetHandle())) { };
+	~CWindowDC() { ReleaseDC(m_lpWindow->GetHandle(), GetHandle()); }
+};
+
+class CDoubleBufferedDC : public CDC
+{
+private:
+	HBITMAP m_hMemBitmap;
+	CDC & m_dcParent;
+	BOOL m_fCompleted;
+	CWindowHandle * m_lpWindow;
+	HGDIOBJ m_hOriginal;
+	SIZE m_szSize;
+
+public:
+	CDoubleBufferedDC(CWindowHandle * lpWindow, CDC & dcParent) :
+		m_dcParent(dcParent),
+		m_fCompleted(FALSE),
+		m_lpWindow(lpWindow),
+		CDC(CreateDC(dcParent))
+	{
+		RECT rc;
+		m_lpWindow->GetWindowRect(&rc);
+		m_szSize.cx = rc.right - rc.left;
+		m_szSize.cy = rc.bottom - rc.top;
+
+		m_hMemBitmap = CreateCompatibleBitmap(m_dcParent.GetHandle(), m_szSize.cx, m_szSize.cy);
+		m_hOriginal = SelectObject(m_hMemBitmap);
+	}
+	virtual ~CDoubleBufferedDC() { Complete(); }
+
+	void Complete() {
+		if (!m_fCompleted) {
+			m_fCompleted = TRUE;
+			POINT pt = { 0, 0 };
+			m_dcParent.BitBlt(pt, m_szSize, this, pt, SRCCOPY);
+			SelectObject(m_hOriginal);
+			DeleteObject(m_hMemBitmap);
+			DeleteDC(GetHandle());
+		}
+	}
+
+private:
+	static HDC CreateDC(CDC & dcParent) {
+		return CreateCompatibleDC(dcParent.GetHandle());
 	}
 };
 
