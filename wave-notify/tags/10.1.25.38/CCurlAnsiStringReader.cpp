b818ddcbe15f73be3d3b3ebb84d6650ae0551eef
pvginkel <pvginkel@7609e39e-0f13-11df-9e5d-c9c78cbd7d0e>
2010-01-25 19:24:41 +0000

    * Created version 10.1.25.38.

---
+++ wave-notify/tags/10.1.25.38/CCurlAnsiStringReader.cpp
@@ -0,0 +1,60 @@
+/*
+ * This file is part of Google Wave Notifier.
+ *
+ * Google Wave Notifier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Google Wave Notifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Google Wave Notifier.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "stdafx.h"
+#include "include.h"
+
+BOOL CCurlAnsiStringReader::Read(LPBYTE lpData, DWORD cbData)
+{
+	ASSERT(lpData != NULL && cbData > 0);
+
+	BOOL fResult = FALSE;
+
+	// Converts using ISO-8859-1 codepage.
+
+	DWORD dwSize = MultiByteToWideChar(28591, 0, (LPCSTR)lpData, cbData, NULL, 0);
+	
+	if (dwSize > 0)
+	{
+		if (dwSize + 1 > m_cbBuffer)
+		{
+			m_cbBuffer = dwSize + 1;
+
+			if (m_szBuffer == NULL)
+			{
+				m_szBuffer = (LPWSTR)malloc(sizeof(WCHAR) * m_cbBuffer);
+			}
+			else
+			{
+				m_szBuffer = (LPWSTR)realloc(m_szBuffer, sizeof(WCHAR) * m_cbBuffer);
+			}
+		}
+
+		dwSize = MultiByteToWideChar(28591, 0, (LPCSTR)lpData, cbData, m_szBuffer, m_cbBuffer);
+
+		if (dwSize > 0)
+		{
+			m_szBuffer[dwSize] = L'\0';
+
+			m_szResult << m_szBuffer;
+
+			fResult = TRUE;
+		}
+	}
+
+	return fResult;
+};
---
+++ wave-notify/tags/10.1.25.38/CWaveRequestGetContactDetails.cpp
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Google Wave Notifier.
+ *
+ * Google Wave Notifier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Google Wave Notifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Google Wave Notifier.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "stdafx.h"
+#include "include.h"
+#include "wave.h"
+
+void CWaveRequestGetContactDetails::CreateRequest(Json::Value & vRoot)
+{
+	vRoot[L"1000"] = Json::Value(Json::arrayValue);
+	vRoot[L"1000"][0u] = Json::Value(0);
+	vRoot[L"1000"][1] = Json::Value(1);
+
+	vRoot[L"2"] = Json::Value(Json::arrayValue);
+
+	UINT nOffset = 0;
+
+	for (TStringVectorIter iter = m_vEmailAddresses.begin(); iter != m_vEmailAddresses.end(); iter++)
+	{
+		vRoot[L"2"][nOffset] = Json::Value(*iter);
+
+		nOffset++;
+	}
+}
