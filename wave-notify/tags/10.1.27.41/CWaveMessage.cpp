fe33d206c8cd46a2db47f170d3f95c62c81973f9
pvginkel <pvginkel@7609e39e-0f13-11df-9e5d-c9c78cbd7d0e>
2010-01-27 12:14:57 +0000

    * Created version 10.1.27.41.

---
+++ wave-notify/tags/10.1.27.41/CWaveMessage.cpp
@@ -0,0 +1,82 @@
+/*
+ * This file is part of Google Wave Notifier.
+ *
+ * Google Wave Notifier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Google Wave Notifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Google Wave Notifier.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "stdafx.h"
+#include "include.h"
+#include "wave.h"
+
+CWaveMessage::CWaveMessage() :
+	m_uContactId(0),
+	m_uOrder(0)
+{
+}
+
+CWaveMessage::CWaveMessage(const CWaveMessage & _Other)
+{
+	m_szText = _Other.m_szText;
+	m_szEmailAddress = _Other.m_szEmailAddress;
+	m_uContactId = _Other.m_uContactId;
+	m_uOrder = _Other.m_uOrder;
+}
+
+CWaveMessage * CWaveMessage::CreateFromJson(Json::Value & vRoot, UINT uOrder)
+{
+	CWaveMessage * lpResult = new CWaveMessage();
+
+// (( scope ))
+{
+	if (!vRoot.isObject())
+	{
+		goto __failure;
+	}
+
+	Json::Value & vText = vRoot[L"1"];
+	Json::Value & vContactId = vRoot[L"6"];
+
+	if (
+		!vText.isString() ||
+		!vContactId.isIntegral()
+	) {
+		goto __failure;
+	}
+
+	lpResult->m_szText = vText.asString();
+	lpResult->m_uContactId = vContactId.asUInt();
+	lpResult->m_uOrder = uOrder;
+
+	return lpResult;
+}
+
+__failure:
+	LOG("Could not parse json");
+
+	delete lpResult;
+
+	return NULL;
+}
+
+void CWaveMessage::ResolveContact(CWave * lpWave)
+{
+	ASSERT(lpWave != NULL);
+
+	const TStringVector & vContacts = lpWave->GetContacts();
+
+	if (m_uContactId < vContacts.size())
+	{
+		m_szEmailAddress = vContacts[m_uContactId];
+	}
+}
---
+++ wave-notify/tags/10.1.27.41/unreadwave.h
@@ -0,0 +1,112 @@
+/*
+ * This file is part of Google Wave Notifier.
+ *
+ * Google Wave Notifier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Google Wave Notifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Google Wave Notifier.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _INC_UNREADWAVE
+#define _INC_UNREADWAVE
+
+#pragma once
+
+typedef enum
+{
+	WCHS_REFRESH,
+	WCHS_IGNORE,
+	WCHS_CHANGED,
+	WCSH_MAX
+} WAVE_CHANGED_STATUS;
+
+class CUnreadWave;
+
+typedef map<wstring, CUnreadWave *> TUnreadWaveMap;
+typedef TUnreadWaveMap::iterator TUnreadWaveMapIter;
+typedef TUnreadWaveMap::const_iterator TUnreadWaveMapConstIter;
+typedef vector<CUnreadWave *> TUnreadWaveVector;
+typedef TUnreadWaveVector::iterator TUnreadWaveVectorIter;
+typedef TUnreadWaveVector::const_iterator TUnreadWaveVectorConstIter;
+
+class CUnreadWave
+{
+private:
+	wstring m_szId;
+	wstring m_szContact;
+	CWaveContact * m_lpContact;
+	wstring m_szSubject;
+	wstring m_szBody;
+	CDateTime m_dtTime;
+	UINT m_uUnread;
+
+public:
+	CUnreadWave(CWave * lpWave, CWaveMessage * lpMessage);
+	virtual ~CUnreadWave() { }
+
+	wstring GetID() const { return m_szId; }
+	wstring GetContact() const { return m_szContact; }
+	wstring GetSubject() const { return m_szSubject; }
+	wstring GetBody() const { return m_szBody; }
+	CDateTime GetTime() const { return m_dtTime; }
+	UINT GetUnread() const { return m_uUnread; }
+	CWaveContact * GetWaveContact() const { return m_lpContact; }
+	void SetWaveContact(CWaveContact * lpContact) { m_lpContact = lpContact; }
+
+	INT Paint(CDC & dc, RECT & rcClient, BOOL fMouseOver, BOOL fExcluseCloseButton) const;
+	INT CalculateHeight(RECT & rcClient) const;
+};
+
+class CUnreadWaveCollection
+{
+private:
+	TUnreadWaveMap m_vUnreadsMap;
+	TUnreadWaveVector m_vUnreadsVector;
+
+private:
+	CUnreadWaveCollection(CWaveCollection * lpLastReported, CWaveCollection * lpCurrent);
+
+public:
+	virtual ~CUnreadWaveCollection();
+
+	const TUnreadWaveVector & GetChanges() const { return m_vUnreadsVector; }
+	CUnreadWave * GetChange(wstring szId) const {
+		CHECK_NOT_EMPTY(szId);
+
+		TUnreadWaveMapConstIter pos = m_vUnreadsMap.find(szId);
+
+		return pos == m_vUnreadsMap.end() ? NULL : pos->second;
+	}
+	CUnreadWave * GetChange(DWORD dwId) const {
+		ASSERT(dwId < m_vUnreadsVector.size());
+
+		return m_vUnreadsVector[dwId];
+	}
+	INT Find(CUnreadWave * lpUnread) const;
+	void DetachAll() {
+		m_vUnreadsMap.clear();
+		m_vUnreadsVector.clear();
+	}
+
+	static CUnreadWaveCollection * CreateUnreadWaves(CWaveCollection * lpLastReported, CWaveCollection * lpCurrent) {
+		return new CUnreadWaveCollection(lpLastReported, lpCurrent);
+	}
+
+private:
+	void InsertAllWaves(CWaveCollection * lpCurrent);
+	void InsertChangesOnly(CWaveCollection * lpLastReported, CWaveCollection * lpCurrent);
+	void Insert(CUnreadWave * lpUnread);
+	CUnreadWave * GetDifference(CWave * lpReportedWave, CWave * lpNewWave) const;
+	BOOL WavesEqual(CWave * lpReportedWave, CWave * lpNewWave) const;
+	WAVE_CHANGED_STATUS GetChangedStatus(CWave * lpReportedWave, CWave * lpNewWave) const;
+};
+
+#endif // _INC_UNREADWAVE
